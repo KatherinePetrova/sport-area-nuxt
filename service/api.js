e96b82aff14c19e8a60b2b0f3b27086bf3bb34f1
KatherinePetrova <zetsu9@mail.ru>
2019-08-19 19:47:01 +0600

    HTTPS AXIOS revert

--- service/api.js
+++ service/api.js
@@ -3,9 +3,9 @@ import https from "https";
 
 export default () => {
   return axios.create({
-    // httpsAgent: new https.Agent({
-    //   rejectUnauthorized: false
-    // }),
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false
+    }),
     baseURL: "http://80.241.42.214:8000/api/"
   });
 };
